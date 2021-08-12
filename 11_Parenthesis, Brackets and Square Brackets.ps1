9855fd64d89052f282c22fb02ebf3a9357c5a472
Tim Davis <binary.god@gmail.com>
2021-08-12 12:41:55 -0400

    Added Session 11

---
+++ 11_Parenthesis, Brackets and Square Brackets.ps1
@@ -0,0 +1,16 @@
+# -------------------------------------------------------
+# Parenthesis
+# -------------------------------------------------------
+# Means evaluate the contents NOW!
+
+# -------------------------------------------------------
+# Brackets
+# -------------------------------------------------------
+# Means evaluation happens when it's their turn
+
+# -------------------------------------------------------
+# Square Brackets
+# -------------------------------------------------------
+# Used to retrieve elements from arrays or hashtables and
+# designate optional params/attributes/type casting
+
